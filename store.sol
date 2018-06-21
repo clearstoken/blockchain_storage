a47b7c2fd21a9ba3f9e92ca470d2e410fbb1a3b2
clearstoken <37632025+clearstoken@users.noreply.github.com>
2018-06-21 09:05:18 -0400

    Create store.sol

---
+++ store.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.6;
+
+contract ClearsKYC {
+    string transactionType;
+    string kycData;
+
+    function set(string transactionType, string kycData) public {
+        
+    }
+
+}
